30eb68f4ccd1bf7eed74d9e31309069bcd62e55c
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-25 22:06:14 +0100

    Create constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const BASE_GATEWAY_URL = "wss://api.guilded.gg/socket.io";
+
+export const BASE_URL = "https://www.guilded.gg/api";
+
+export const IMAGE_BASE_URL = "https://s3-us-west-2.amazonaws.com/www.guilded.gg";
+
+export const baseEndpoints = {
+  BASE_GATEWAY_URL,
+  BASE_URL,
+  CDN_URL: IMAGE_BASE_URL,
+};
