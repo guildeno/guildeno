95fcc372b1aca50333ae3fc0b4bc72443cd78c33
ITOH <to@itoh.at>
2022-05-10 22:49:40 +0200

    website changes

---
+++ website/src/components/HomepageFeatures.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./HomepageFeatures.module.css";
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <h3><strong>EMPTY ➔ MPTY ➔ M TY ➔ M T ➔</strong></h3>
+      </div>
+    </section>
+  );
+}
